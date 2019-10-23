de0a9fdecf45acb59ab59451d12101a504febac0
Yathi <511386+yuth@users.noreply.github.com>
2019-10-23 13:03:39 -0700

    change subscription parameter name

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onNewMessage = `subscription OnNewMessage($groupId: ID!) {
-  onNewMessage(groupId: $groupId) {
+export const onNewMessage = `subscription OnNewMessage($messageGroupId: ID!) {
+  onNewMessage(messageGroupId: $messageGroupId) {
     id
     message
     group {
